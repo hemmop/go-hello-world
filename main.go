96ce001964cb6da6e891ace4f2e1f0dcf6db8e1e
Per Carlson <pelle@hemmop.com>
2016-02-16 14:36:16 +0100

    Log request

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", r.URL.Path[1:])
+    fmt.Println("request: method=", r.Method, " URL=", r.URL, " Proto=", r.Proto, " Remote=", r.RemoteAddr)
+    fmt.Fprintf(w, "Hello World! %s", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
